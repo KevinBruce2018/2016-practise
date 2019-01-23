e37b5387565aefbc948612b6ea14a3901853385a
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2019-01-23 12:00:53 +0800

    Create Left-pad.cpp

---
+++ Left-pad.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    char c;
+    string s;
+    cin>>n>>c;
+    getchar();//该死的回车必须要清理掉啊
+    getline(cin,s);
+    if(s.length()>=n)
+        for(int i = s.length()-n;i < s.length();i++)
+            cout<<s[i];
+    else
+    {
+        for(int i = n-s.length();i > 0;i--)
+            cout<<c;
+        for(int i = 0;i < s.length();i++)
+            cout<<s[i];
+    }
+    cout<<endl;
+    return 0;
+}
