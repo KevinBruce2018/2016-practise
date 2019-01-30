f20bb8ea70d40adc4efb9b2b0ab57f4c14a6fa4e
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2019-01-30 14:15:02 +0800

    Create 情人节.cpp

---
+++ 情人节.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char name[100][11];
+	int i;
+
+    //过于简单 没话说……
+	if(i< 2 )
+		printf("Momo... No one is for you ...\n");
+	else if(i < 14)
+		printf("%s is the only one for you...\n",name[1]);
+	else
+		printf("%s and %s are inviting you to dinner...\n",name[1],name[13]);
+	return 0;
+}
